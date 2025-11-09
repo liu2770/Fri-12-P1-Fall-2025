39b012297a7c9f08520e5018e0e1e8c66b71f90d
Krysten-Scase <krysten.scase@gmail.com>
2025-11-09 17:57:57 -0500

    Update lookUpProducts.py

--- src/p1_functions/lookUpProducts.py
+++ src/p1_functions/lookUpProducts.py
@@ -1,4 +1,21 @@
 def lookup_products(products):
-    #TODO: put your code here
-    print("testing!")   #!Remove this line, stub
-    return
+    file=open("./content/user.csv","r")
+        items = products.split(",")
+    list = []
+    products_file = []
+    for line in file:
+        line_copy = line
+        line_copy = line_copy.replace("\n","")
+        products_file.append(line_copy.split(","))
+    for name in items:
+        match = False
+        for index in range(len(products_file)):
+            if name == products_file[index][0]:
+                match = True
+                index_match = index
+        if match == True:
+            list.append(products_file[index_match])
+        elif match == False:
+            print(f"Error, product '{name}' not found")
+    file.close()
+    return list
