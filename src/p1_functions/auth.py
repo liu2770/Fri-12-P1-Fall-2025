c1ca857c95945b15f56f14d0665182a72c882e55
Jincheng Liu <Jincheng_L@hotmail.com>
2025-11-12 23:06:56 -0500

    Updated auth.py to catch up on signup.py
    - Added .strip() to every relevent user input.
    - Switched from reading files manually to using built in csv module.

--- src/p1_functions/auth.py
+++ src/p1_functions/auth.py
@@ -1,19 +1,19 @@
 from p1_functions.signup import sign_up
 import lib.bcrypt   #included in lib folder
+import csv
 
-def loadUserEntry():
+def loadUserEntry():    #TODO: Use csv module to handle IO instead
     """
     Helper function, handels read from users.csv. Return 
     a 2D list consists of user names and corresponding 
     passwords.
     """
-    file=open("./content/users.csv","r")
     userList=[]
-    fileLines=file.read().split(sep="\n")
-    for line in fileLines:
-        userEntry=line.split(sep=', ')  #!Will broke if sign_up() doesn't follow the exact pattern
-        userList.append(userEntry)
-    file.close()
+    with open("./content/users.csv",newline='') as file:
+        csvReader=csv.reader(file)
+        # fileLines=file.read().split(sep="\n")
+        for userEntry in csvReader:
+            userList.append(userEntry)
     return userList
     
 def authenticate():
@@ -28,15 +28,16 @@ def authenticate():
     #Proceed to authenticate the user with a while loop
     while True:
         #Ask the user to input their user name, if they dont have one they should let the program know by putting "-1"
-        userName=input("Please enter your user name now, enter -1 if you dont have one; quit() for quit: ")  #?Accepts any string at the moment
+        userName=input("Please enter your user name now, enter -1 if you dont have one; quit() for quit: ").strip()  #?Accepts any string at the moment
 
         #check if sign_up call is needed
         if userName=="quit()":  #?Exit 1
-            input("Sign up canceled.")
+            input("Sign up canceled. Enter anything to continue...")
             return None #Incase user just want to quit
         if userName=="-1":
             sign_up()
             input("Sign up complete, enter anything to continue...")
+            userName=input("Please enter your user name now, enter -1 if you dont have one; quit() for quit: ").strip()
         
         #Load the user entry, incase a new user has been added 
         userList=loadUserEntry()
@@ -50,7 +51,7 @@ def authenticate():
                 uesrIndex=userList.index(entry)
 
         if userExist==True:
-            password=input("User found, please input password now: ")
+            password=input("User found, please input password now: ").strip()
             expectedPassword=str(userList[uesrIndex][1])    #The str constrcutor doesn't do anything really, it's just here for readability
             if lib.bcrypt.checkpw(password.encode('utf-8'),expectedPassword.encode('utf-8')):    #?Exit2
                 print(f"User authentication acknowledged, welcome {userName}.")
