37487cb71cebaf0da1680cafd5d9a0e4a50762e6
Krysten-Scase <krysten.scase@gmail.com>
2025-11-13 11:51:43 -0500

    Update lookUpProducts.py
    - Added general comments for clarity
    - Made some changes to variable names for clarity
    - Added code to turn prices into integers and floats (probably just floats was fine, it wont make a different in calculations it is just cleaner when final_list is printed)

--- src/p1_functions/lookUpProducts.py
+++ src/p1_functions/lookUpProducts.py
@@ -1,21 +1,32 @@
-def lookup_products(products):  #TODO:General lack of comments
-    file=open("./content/products.csv","r")
-    items = products.split(",")
-    list = []   #TODO:variable names are hard to distinguish
+def lookup_products(products):
+    '''Returns a 2D list of products and their prices from the inputted string that matched a product in the products.csv file.
+    Outputting an error print if any inputted product names didn't match any in products.csv'''
+    file = open("products.csv") #open file
+    wanted_products = products.split(",") # List of products that were in the string of "products"
+    final_list = []
     products_file = []
-    for line in file:
+
+    for line in file: # Stripping white space from lines and appending to product_file list, making a 2D list of available products
         line_copy = line
-        line_copy = line_copy.replace("\n","")
+        line_copy = line_copy.strip()
         products_file.append(line_copy.split(","))
-    for name in items:
+
+    for index in range(len(products_file)): # Turning price values into integers or float if price isn't a whole number
+        if "." in products_file[index][1]:
+            products_file[index][1] = float(products_file[index][1])
+        else:
+            products_file[index][1] = int(products_file[index][1])
+
+    for name in wanted_products:
         match = False
-        for index in range(len(products_file)):
+        for index in range(len(products_file)): # Checking for matches between list of available products and input string of wanted products
             if name == products_file[index][0]:
                 match = True
                 index_match = index
-        if match == True:   #TODO:redundant if statements
-            list.append(products_file[index_match])
-        elif match == False:
+        if match == True: # Match
+            final_list.append(products_file[index_match]) # Appending final list with matched product list and price
+        elif match == False: # No Match (error)
             print(f"Error, product '{name}' not found")
+
     file.close()
-    return list #?On the P1 documents it says "Make sure the data types are correct." Does it mean we need to make prices into floats?
+    return final_list
