d07dcf0527010576d7a6c8ea1a58b0c9c38a41a4
Jincheng Liu <Jincheng_L@hotmail.com>
2025-11-14 12:05:33 -0500

    Uploaded the new Q-arm Code
    -Updated Q-arm test code to qarmTest.py.

--- src/tests/qarmTest.py
+++ src/tests/qarmTest.py
@@ -2,7 +2,7 @@
 # coding: utf-8
 # --------------------------------------------------------------------------------
 
-import sys
+import sys  #TODO: CHECK THE STYLING AND FUNCTION NAMES
 
 sys.path.append("../")
 
@@ -17,70 +17,132 @@ scan_barcode = BarcodeScanner.scan_barcode
 # STUDENT CODE BEGINS
 # ---------------------------------------------------------------------------------
 #TODO: Redo the skeletal strcuture, keywords: open&close, individual functions for all objects, move to parcel from home
-##Global variables
-action_interval=1   #time in seconds
 ##Functions
 def gripperInit():  #Initialize gripper state
+
     arm.home()
-    sleep(action_interval)
+    sleep(1)
 
     close_gripper()
-    sleep(action_interval)
+    sleep(1)
     return
 
-def open_gripper():
-    arm.rotate_gripper(-120)
-    sleep(action_interval)
+def open_gripper(degree):
+
+    arm.rotate_gripper(-degree)
+    sleep(1)
     return
 
 def close_gripper():
-    arm.rotate_gripper(120)
-    sleep(action_interval)
+
+    arm.rotate_gripper(720)
+    sleep(1)
     return
 
 def move_to_parcel():
+
     #TODO: Store the script for moving the gripper to parcel location
     return
 
 def target_1_logic():   #Sponge
+
     #TODO: Store the script for picking up object 1 and return to home
 
     arm.home()
-    sleep(action_interval)
+    sleep(1)
+    open_gripper(20)
+    sleep(1)
+    arm.rotate_base(15)
+    arm.get_arm_position()
+    sleep(1)
+    arm.rotate_elbow(-10)
+    sleep(1)
+    arm.rotate_shoulder(45)
+
+    close_gripper()
+
+    arm.rotate_shoulder(-45)
+
+    arm.home()
     return
 
 def target_2_logic():   #Bottel
+
     #TODO: Store the script for picking up object 2 and return to home
 
     arm.home()
-    sleep(action_interval)
+    sleep(1)
+    open_gripper(30)
+    sleep(1)
+    arm.rotate_base(8)
+    sleep(1)
+    arm.rotate_elbow(-17)
+    sleep(1)
+    arm.rotate_shoulder(50)
+    sleep(1)
+
+    close_gripper()
+
+    arm.rotate_shoulder(-45)
+
+    arm.home()
+    sleep(1)
     return
 
 def target_3_logic():   #D12
+
     #TODO: Store the script for picking up object 3 and return to home
 
     arm.home()
-    sleep(action_interval)
+    sleep(1)
     return
 
 def target_4_logic():   #Wizard hat
+
     #TODO: Store the script for picking up object 4 and return to home
 
     arm.home()
-    sleep(action_interval)
+    sleep(1)
+    arm.rotate_base(-3)
+    sleep(1)
+    arm.rotate_elbow(-5)
+    sleep(1)
+    arm.rotate_shoulder(40)
+    sleep(1)
+    print(f"Rook position: {arm.get_arm_position()}")
     return
 
 def target_5_logic():   #Bowl
+
     #TODO: Store the script for picking up object 5 and return to home
 
     arm.home()
-    sleep(action_interval)
+    sleep(1)
+    return
+
+def target_6_logic():   #Rook
+
+    arm.home()
+    sleep(1)
+
+    arm.rotate_base(3)
+    sleep(1)
+    arm.rotate_elbow(-5)
+    sleep(1)
+    arm.rotate_shoulder(40)
+    #arm.set_arm_position([0.5563790264878321, 0.028623598527956644, 0.1451919543714052]
+    print(f"Rook position: {arm.get_arm_position()}")
+    sleep(1)
     return
 
 ##Testing starts here
 
 #init
-arm.home()
+gripperInit()
+#list=scan_barcode().split()
+#print(list)
+#target_1_logic()
+target_4_logic()
 
 #TODO: get arm to one of the objects
 #TODO: try get the scooper to go below the object
@@ -88,7 +150,7 @@ arm.home()
 #TODO: test if the gripper can rotate 180 so the object kidna rest on the scooper
 
 #print useful data
-print(arm.get_arm_position())
+#print(arm.get_arm_position())
 
 #TODO: get the arm to the percel loc
 #TODO: drop the object
