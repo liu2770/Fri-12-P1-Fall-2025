da1fb89a27f45acd6a2bda4868557ff15c2bceba
ziruigu11-pixel <ziruigu11@gmail.com>
2025-11-18 09:39:46 -0500

    Update customerSummary.py

--- src/p1_functions/customerSummary.py
+++ src/p1_functions/customerSummary.py
@@ -1,27 +1,34 @@
 def customer_summary(userid):
     file = open("orders.csv",'r')
     validation = False
-    userlines = []
     num_orders = 0
     total_spent = 0
-
+    products = ["Sponge", "Bottle", "Rook", "D12", "Bowl", "WitchHat"]
+    product_count = [0,0,0,0,0,0]
     for line in file:
         line = line.strip().split(",")
         if line[0] == userid:
             validation = True
             num_orders += 1
             total_spent += float(line[1])
+            for p in range (len(products)):
+                for i in range (len(line)):
+                    if line[i] == products[p]:
+                        product_count[p] += 1
 
     if validation is True:
-
         print("===================================")
         print(f" Customer Summary for {userid}")
         print("===================================")
         print(f" Total Orders : {num_orders}")
         print(f" Total Spent  : ${total_spent:.2f}")
+        print("------------ Products -------------")
+
+        for i in range(len(products)):
+            if product_count[i] != 0: #print product quantity if not 0
+                print(f"{products[i]:<15}| {product_count[i]}")
         print("===================================")
 
     else:
         print("Invalid username")
 
-customer_summary("test")
