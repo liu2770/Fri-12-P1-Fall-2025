1a8e9f2786b000b8af95f780d04588cc56faecaa
ziruigu11-pixel <ziruigu11@gmail.com>
2025-11-18 17:10:00 -0500

    Update customerSummary.py

--- src/p1_functions/customerSummary.py
+++ src/p1_functions/customerSummary.py
@@ -1,20 +1,31 @@
 def customer_summary(userid):
     file = open("orders.csv",'r')
+
+    products = []
+    for line in file:
+        line = line.strip().split(",")
+        product_name = line[2]
+        if product_name not in products:
+            products.append(product_name)
+    product_count = [0]*len(products)
+
+
     validation = False
     num_orders = 0
     total_spent = 0
-    products = ["Sponge", "Bottle", "Rook", "D12", "Bowl", "WitchHat"]
-    product_count = [0,0,0,0,0,0]
+
+    file = open("orders.csv",'r')
     for line in file:
         line = line.strip().split(",")
         if line[0] == userid:
             validation = True
             num_orders += 1
             total_spent += float(line[1])
-            for p in range (len(products)):
-                for i in range (len(line)):
-                    if line[i] == products[p]:
-                        product_count[p] += 1
+            for i in range (len(products)):
+                for i2 in range (len(line)):
+                    if line[i2] == products[i]:
+                        product_count[i] += 1
+    file.close()
 
     if validation is True:
         print("===================================")
