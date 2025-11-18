7c5a14a0165d76487ccbfc46c4415dadde297bfa
ziruigu11-pixel <ziruigu11@gmail.com>
2025-11-18 01:39:48 -0500

    Update customerSummary.py

--- src/p1_functions/customerSummary.py
+++ src/p1_functions/customerSummary.py
@@ -1,41 +1,27 @@
 def customer_summary(userid):
-    file = open("./content/orders.csv",'r')
+    file = open("orders.csv",'r')
     validation = False
     userlines = []
+    num_orders = 0
+    total_spent = 0
+
     for line in file:
         line = line.strip().split(",")
         if line[0] == userid:
             validation = True
-            userlines.append(line)
+            num_orders += 1
+            total_spent += float(line[1])
 
     if validation is True:
-        num_orders = len(userlines)  # total number of orders
-        total_spent = 0.0
-        product_count = [0, 0, 0, 0, 0, 0]
-        products = ["Sponge", "Bottle", "Rook", "D12", "Bowl", "WitchHat"]
-
-        for order in userlines:
-            product = str(order[1])
-            qty = int(order[2])
-            price = float(order[3])
-            total_spent += qty * price  # total spent of orders
-
-            for i in range(len(products)):  #store quantity of each product into the list
-                if product == products[i]:
-                    product_count[i] += qty
 
         print("===================================")
         print(f" Customer Summary for {userid}")
         print("===================================")
         print(f" Total Orders : {num_orders}")
         print(f" Total Spent  : ${total_spent:.2f}")
-        print("------------ Products -------------")
-
-        for i in range(len(products)):
-            if product_count[i] != 0:   #print product quantity if not 0
-                print(f"{products[i]}             | {product_count[i]}")
-                
         print("===================================")
 
     else:
         print("Invalid username")
+
+customer_summary("test")
