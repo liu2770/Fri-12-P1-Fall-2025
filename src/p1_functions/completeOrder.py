546b30ce31c01a93889b1bb5f2d566d9aa646c07
manandhs <sanilmanandhar07@gmail.com>
2025-11-19 18:04:09 -0500

    Update completeOrder.py

--- src/p1_functions/completeOrder.py
+++ src/p1_functions/completeOrder.py
@@ -50,16 +50,16 @@ def complete_order(userid, product_list):
     print(order_count)
 
     print("\n   FRI-12 Warehouse   ")   #print statements that print formatted reciept 
-    print(f"-" * 22)
+    print(f"-" * 25)
 
     for i in range(len(product_list)):   #for loop that adds orders and price  for reciept
         product_name = product_list[i][0]
         product_price = product_list[i][1]
-        print(f"{product_name:<11}{product_price:>13.2}")
+        print(f"{product_name:<11}${product_price:>13.2}")
 
-    print(f"-" * 22)
-    print(f"{"Subtotal":<10}{subtotal:>15}")
-    print(f"{"Discount":<10}{discount:>15.2f}")
-    print(f"{"Tax":<10}{tax_applied:>15.2f}")
-    print(f"{"Total":<10}{total:>15}")
+    print(f"-" * 25)
+    print(f"{"Subtotal":<10}${subtotal:>15}")
+    print(f"{"Discount":<10}${discount:>15.2f}")
+    print(f"{"Tax":<10}${tax_applied:>15.2f}")
+    print(f"{"Total":<10}${total:>15}")
     print(f"\nyou have made {order_count} orders with us so far ")
