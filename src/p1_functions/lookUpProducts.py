e50312b68cfedc79abefe9971e9dab4053fae6d5
Jincheng Liu <Jincheng_L@hotmail.com>
2025-11-21 14:14:58 -0500

    Changes to lookUpProducts.py
    - lookup_products() now splits the incoming string by whitespace instead
      of comma.

--- src/p1_functions/lookUpProducts.py
+++ src/p1_functions/lookUpProducts.py
@@ -2,7 +2,7 @@ def lookup_products(products):
     '''Returns a 2D list of products and their prices from the inputted string that matched a product in the products.csv file.
     Outputting an error print if any inputted product names didn't match any in products.csv'''
     file = open("./content/products.csv") #open file
-    wanted_products = products.split(",") # List of products that were in the string of "products"
+    wanted_products = products.split(" ") # List of products that were in the string of "products"
     final_list = []
     products_file = []
 
