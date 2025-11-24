10412666a24bd31cba1f64fd6bbdc28cd921af35
Jincheng Liu <Jincheng_L@hotmail.com>
2025-11-23 20:25:32 -0500

    Implementing PackProduct Stage 1
    - Migrated the code from qarmTest to packProducts
    - Cleaned up comments
    - Commented out actual implementation of packProducts in main out for
      testing purpose

--- src/main.py
+++ src/main.py
@@ -7,7 +7,8 @@ from p1_functions.signup import sign_up
 from p1_functions.completeOrder import complete_order
 from p1_functions.customerSummary import customer_summary
 from p1_functions.lookUpProducts import lookup_products
-from p1_functions.packProducts import pack_products
+#from p1_functions.packProducts import pack_products    #!UNCOMMENT BEFORE DEMO
+#from Common.qarm_interface_wrapper import *    #!UNCOMMENT BEFORE DEMO
 import os
 
 #Setting up the work directory to where ever the main.py is, useful for file paths
@@ -15,37 +16,25 @@ location=os.path.dirname(__file__)
 os.chdir(location)
 
 ##*Code starts here
-#TODO: Implement a skeletal structure following the outlined sequence of events
-#TODO: Discuss and further refine the styles of the outputs
+##Welcome the user
+print('='*30)
+print("Welcome to Fri-12 Warehouse")
+print('='*30+'\n')
+
+##Login the user  
+user=authenticate()
+
 while True:
-    ##Welcome the user
-    #TODO:print welcome message
-    print('='*30)
-    print("Welcome to Fri-12 Warehouse")
-    print('='*30+'\n')
-
-    ##Login the user  
-    user=authenticate()
-    if user==None:
-        print("Order canceled")
-        break
-
     ##Scans items   
-    #scan_barcode()
-    scanned_list="D12,Sponge,WitchHat"
+    #scanned_list=BarcodeScanner.scan_barcode()    #!UNCOMMENT BEFORE DEMO
+    scanned_list="D12 Sponge WitchHat test"
 
     ##Looks up items and their prices
     product_list=lookup_products(scanned_list)
+    print(f"product_list: {product_list}")
 
-    ##Order to Q-arm to move the items
-    # product_list = [["D12",0.12],["Sponge",0.50]]
-    # file = open("./content/products.csv", "r")
-    # for line in file:
-    #     product = []
-    #     for word in line.split(","):
-    #         product.append(word)
-    #     product_list.append(product)
-    pack_products(product_list) 
+    ##Order the Q-arm to move the items
+    #pack_products(product_list)    #!UNCOMMENT BEFORE DEMO
 
     ##Calculate total & record order in csv
     complete_order(user,product_list)
@@ -55,4 +44,4 @@ while True:
         break
 
 ##Summarize customer order history
-customer_summary(input("Please enter the user ID to see its account summary: "))    #!Need a proper input handling here
+customer_summary(user)
--- src/p1_functions/packProducts.py
+++ src/p1_functions/packProducts.py
@@ -1,20 +1,168 @@
-def pack_products(product_list):
-    for product in product_list:
-        if product in product_list:
-            ##Product moving code
-            print(f"{product[0]} has been packed")
-        else:
-            print("The product you are looking for is not available, please try again")
-    print("testing!")   #!Remove this line, stub
+# !/usr/bin/env python3
+# coding: utf-8
+# --------------------------------------------------------------------------------
+
+import sys
+
+sys.path.append("../")
+
+from time import sleep
+from Common.qarm_interface_wrapper import *
+
+def pack_logic_handler(arm,item):
+    gripperInit(arm)
+    match item:
+        case "Sponge":
+            target_1_logic(arm)
+            pass
+        case "Bottle":
+            target_2_logic(arm)
+            pass
+        case "D12":
+            target_3_logic(arm)
+            pass
+        case "Rook":
+            target_6_logic(arm)
+            pass
+        case "WitchHat":
+            target_4_logic(arm)
+            pass
+        case "Bowl":
+            target_5_logic(arm)
+            pass
     return
 
-# product_list = []
 
-# file = open("./content/products.csv", "r")
+def pack_products(product_list):
+    GRIPPER_IMPLEMENTATION = 1
+    arm = QArmInterface(GRIPPER_IMPLEMENTATION)
 
-# for line in file:
-#     product = []
-#     for word in line.split(","):
-#         product.append(word)
+    for product in product_list:
+        pack_logic_handler(arm,product[0])
+        print(f"{product[0]} has been packed")
 
-# print(product_list)
+    print("All items packed successfully")
+
+    arm.end_arm_connection()
+    return
+
+def gripperInit(arm):  #Initialize gripper state
+
+    arm.home()
+    sleep(1)
+
+    close_gripper()
+    sleep(1)
+    return
+
+def open_gripper(arm,degree):
+
+    arm.rotate_gripper(-degree)
+    sleep(1)
+    return
+
+def close_gripper(arm):
+
+    arm.rotate_gripper(720)
+    sleep(1)
+    return
+
+def move_to_parcel(arm):
+    arm.set_arm_position([0.22503238356190008, -0.23900971615063057, 0.22808881742811488])
+    open_gripper(arm,40)
+    return
+
+def target_1_logic(arm):   #Sponge
+    open_gripper(arm,20)
+    sleep(1)
+    arm.set_arm_position([0.5490539443478295, 0.15789464150191637, 0.19156143380856633])    #Get Q-arm to the space above the obect
+    sleep(1)
+    
+    arm.rotate_shoulder(5)  #Final Descend to grab the object
+    sleep(1)
+    arm.rotate_elbow(-2)
+    sleep(1)
+    close_gripper(arm)
+    sleep(1)
+
+    arm.rotate_shoulder(-45)    #Bring the Q-arm up
+    sleep(1)
+
+    move_to_parcel(arm)
+
+    arm.home()
+    return
+
+def target_2_logic(arm):   #Bottle
+
+    open_gripper(15)
+    arm.set_arm_position([0.5850203821304305, 0.07719062354168196, 0.05075644615654784])
+    sleep(1)
+
+    close_gripper()
+    sleep(1)
+
+    arm.rotate_shoulder(-45)
+    sleep(1)
+
+    move_to_parcel()
+
+    arm.home()
+    return
+
+def target_6_logic(arm):   #Rook
+    open_gripper(15)
+    arm.set_arm_position([0.5714053048456229, 0.01622402562232702, 0.08827264038887025])
+    sleep(1)
+
+    close_gripper()
+    sleep(1)
+
+    arm.rotate_shoulder(-50)
+    sleep(1)
+
+    move_to_parcel()
+    return
+
+##!BELOW NEEDS UNIQUE POSITION
+def target_3_logic(arm):   #D12
+    open_gripper(15)
+    arm.set_arm_position([0.5714053048456229, 0.01622402562232702, 0.08827264038887025])    #!NEED UNIQUE POSITION
+    sleep(1)
+
+    close_gripper()
+    sleep(1)
+
+    arm.rotate_shoulder(-50)
+    sleep(1)
+
+    move_to_parcel()
+    return
+
+def target_4_logic(arm):   #Wizard hat
+    open_gripper(15)
+    arm.set_arm_position([0.5714053048456229, 0.01622402562232702, 0.08827264038887025])    #!NEED UNIQUE POSITION
+    sleep(1)
+
+    close_gripper()
+    sleep(1)
+
+    arm.rotate_shoulder(-50)
+    sleep(1)
+
+    move_to_parcel()
+    return
+
+def target_5_logic(arm):   #Bowl
+    open_gripper(15)
+    arm.set_arm_position([0.5714053048456229, 0.01622402562232702, 0.08827264038887025])    #!NEED UNIQUE POSITION
+    sleep(1)
+
+    close_gripper()
+    sleep(1)
+
+    arm.rotate_shoulder(-50)
+    sleep(1)
+
+    move_to_parcel()
+    return
