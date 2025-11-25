afc1f1c764c6cf676a43fda7eeaf59dfd5c652e5
Jincheng Liu <Jincheng_L@hotmail.com>
2025-11-25 10:10:03 -0500

    Updated single file version of the program
    - Updated the single file version of the program to be up to date with
      recent changes

--- demo_ready_file/Fri-12_P1.py
+++ demo_ready_file/Fri-12_P1.py
@@ -1,19 +1,31 @@
+# !/usr/bin/env python3
+# coding: utf-8
+# --------------------------------------------------------------------------------
+
 """
 The main file for P1 Q-arm program. This version combines
 all files into one in case there is issues with loading the
 normal file structure.
 Fri-12 P1, 2025 Fall
 """
+import os
+import sys
 import csv
 import bcrypt
+import random
 import re
 
-#Functions
-def sign_up():
+sys.path.append("../")
+
+from time import sleep
+# from Common.qarm_interface_wrapper import * #!UNCOMMENT BEFORE DEMO
+
+##*Functions
+def sign_up():  #?Changes required
     # ask for user id and clean spaces
     userid = input("Enter a new user ID: ").strip()
     # read existing users
-    with open("content/users.csv", "r", newline="") as f:
+    with open("users.csv", "r", newline="") as f:
         reader = csv.reader(f)
         existing = [row[0] for row in reader]
     if userid in existing:
@@ -32,22 +44,22 @@ def sign_up():
         else:
             print("Password doesn't meet requirements, try again.")
     hashed_pw = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt()).decode("utf-8")
-    with open("content/users.csv", "a", newline="") as f:
+    with open("users.csv", "a", newline="") as f:
         writer = csv.writer(f)
         writer.writerow([userid, hashed_pw])
 
     print("Account created.")
 
-def loadUserEntry():   
+def loadUserEntry(): 
     """
     Helper function, handels read from users.csv. Return 
     a 2D list consists of user names and corresponding 
     passwords.
+    Jincheng Liu
     """
     userList=[]
-    with open("users.csv",newline='') as file:
+    with open("users.csv",newline="") as file:   #using "with" to better handle file IO (close automatically); csv reader eliminates \r (carriage return) and newline statement removes \n at the end
         csvReader=csv.reader(file)
-        # fileLines=file.read().split(sep="\n")
         for userEntry in csvReader:
             userList.append(userEntry)
     return userList
@@ -59,6 +71,7 @@ def authenticate():
     if they dont have one call sign_up(). Return
     the user name upon successful authentication,
     let them keep trying if not. Reads from users.csv.
+    Jincheng Liu
     """
     ##Start of the program
     #Proceed to authenticate the user with a while loop
@@ -93,15 +106,15 @@ def authenticate():
                 print(f"User authentication acknowledged, welcome {userName}.")
                 return userName
             else:
-                input("Password does not match, enter anything to try again...")    #use input to pause the program so user can read the output
+                input("Password does not match, enter anything to try again...\n")    #use input to pause the program so user can read the output
         else:
             input("User not found, enter anything to try again...")
 
-def lookup_products(products):
+def lookup_products(products):  #?Latest
     '''Returns a 2D list of products and their prices from the inputted string that matched a product in the products.csv file.
-    Outputting an error print if any inputted product names didn't match any in products.csv'''
+    Outputting an error print if any inputted product names didn't match any in products.csv. Made by KRYSTEN SCASE, 400628749'''
     file = open("products.csv") #open file
-    wanted_products = products.split(",") # List of products that were in the string of "products"
+    wanted_products = products.split(" ") # List of products that were in the string of "products"
     final_list = []
     products_file = []
 
@@ -130,47 +143,259 @@ def lookup_products(products):
     file.close()
     return final_list
 
+#Line 146-300, helper function and pack_product proper starts
+def pack_logic_handler(arm,item):
+    gripperInit(arm)
+    match item:
+        case "Sponge":
+            target_1_logic(arm)
+            pass
+        case "Bottle":
+            target_2_logic(arm)
+            pass
+        case "D12":
+            target_3_logic(arm)
+            pass
+        case "Rook":
+            target_6_logic(arm)
+            pass
+        case "WitchHat":
+            target_4_logic(arm)
+            pass
+        case "Bowl":
+            target_5_logic(arm)
+            pass
+    return
+
 def pack_products(product_list):
+    GRIPPER_IMPLEMENTATION = 1
+    arm = QArmInterface(GRIPPER_IMPLEMENTATION)
+
     for product in product_list:
-        if product in product_list:
-            ##Product moving code
-            print(f"{product[0]} has been packed")
-        else:
-            print("The product you are looking for is not available, please try again")
-    print("testing!")   #!Remove this line, stub
+        pack_logic_handler(arm,product[0])
+        print(f"{product[0]} has been packed")
+
+    print("All items packed successfully")
+
+    arm.end_arm_connection()
     return
 
-def complete_order(userid, product_list):
-    #TODO: put your code here
-    print("testing!")   #!Remove this line, stub
+def gripperInit(arm):  #Initialize gripper state
+
+    arm.home()
+    sleep(1)
+
+    close_gripper()
+    sleep(1)
     return
 
-def customer_summary(userid):
-    file = open("orders.csv",'r')
+def open_gripper(arm,degree):
+
+    arm.rotate_gripper(-degree)
+    sleep(1)
+    return
+
+def close_gripper(arm):
+
+    arm.rotate_gripper(720)
+    sleep(1)
+    return
+
+def move_to_parcel(arm):
+    arm.set_arm_position([0.22503238356190008, -0.23900971615063057, 0.22808881742811488])
+    open_gripper(arm,40)
+    return
+
+def target_1_logic(arm):   #Sponge
+    open_gripper(arm,20)
+    sleep(1)
+    arm.set_arm_position([0.5490539443478295, 0.15789464150191637, 0.19156143380856633])    #Get Q-arm to the space above the obect
+    sleep(1)
+    
+    arm.rotate_shoulder(5)  #Final Descend to grab the object
+    sleep(1)
+    arm.rotate_elbow(-2)
+    sleep(1)
+    close_gripper(arm)
+    sleep(1)
+
+    arm.rotate_shoulder(-45)    #Bring the Q-arm up
+    sleep(1)
+
+    move_to_parcel(arm)
+
+    arm.home()
+    return
+
+def target_2_logic(arm):   #Bottle
+
+    open_gripper(15)
+    arm.set_arm_position([0.5850203821304305, 0.07719062354168196, 0.05075644615654784])
+    sleep(1)
+
+    close_gripper()
+    sleep(1)
+
+    arm.rotate_shoulder(-45)
+    sleep(1)
+
+    move_to_parcel()
+
+    arm.home()
+    return
+
+def target_6_logic(arm):   #Rook
+    open_gripper(15)
+    arm.set_arm_position([0.5714053048456229, 0.01622402562232702, 0.08827264038887025])
+    sleep(1)
+
+    close_gripper()
+    sleep(1)
+
+    arm.rotate_shoulder(-50)
+    sleep(1)
+
+    move_to_parcel()
+    return
+
+def target_3_logic(arm):   #D12
+    open_gripper(15)
+    arm.set_arm_position([0.5694053048456229, -0.01622402562232702, 0.08827264038887025])
+    sleep(1)
+
+    close_gripper()
+    sleep(1)
+
+    arm.rotate_shoulder(-45)
+    sleep(1)
+
+    move_to_parcel()
+    return
+
+def target_4_logic(arm):   #Wizard hat
+    open_gripper(15)
+    arm.set_arm_position([0.5754053048456229, -0.07422402562232702, 0.09127264038887025])
+    sleep(1)
+
+    close_gripper()
+    sleep(1)
+
+    arm.rotate_shoulder(-45)
+    sleep(1)
+
+    move_to_parcel()
+    return
+
+def target_5_logic(arm):   #Bowl
+    open_gripper(15)
+    arm.set_arm_position([0.5604053048456229, -0.15789464150191637, 0.08527264038887025])
+    sleep(1)
+
+    close_gripper()
+    sleep(1)
+
+    arm.rotate_shoulder(-50)
+    sleep(1)
+
+    move_to_parcel()
+    return
+#Line 146-300, helper function and pack_product proper ends
+
+def complete_order(userid, product_list):   #?Changes Required
+    tax = 0.13   #sets tax to 0.13 percent
+    random_discount = random.uniform(0.05,0.50)   #generates random float value between 0.05 and 0.50 for discount
+
+    subtotal = 0   #sets subtotal to 0
+
+    product = []   #empty list to store product names
+
+    for i in range(len(product_list)):   #for loop that appends product names from products.csv to product list
+        product_names = product_list[i][0]
+        product.append(product_names)
+    ordered_products = ", ".join(product)
+
+    print(ordered_products)
+
+    for i in range(len(product_list)):   #for loop that calculates order subtotal based on price of ordered products
+        price = float(product_list[i][1])
+        subtotal += price
+
+    discount = subtotal * random_discount   #calculates discount based on subtotal and random discoun value
+
+    pre_tax_total = subtotal - discount   #calculates pre-tax price of order
+
+    tax_applied = pre_tax_total * tax   #calculates tax on order
+
+    total = round(pre_tax_total + tax_applied, 2)   #calculates order total and rounds it to 2 decimal places
+
+    total = str(total)
+
+    order_count = 0   #initializes order count to keep track of customer orders
+
+    filename = "orders.csv"   #sets filename to orders.csv so it can be opened later
+
+    file = open(filename, "r")   #opens orders.csv to read from
+
+    for line in file:   # for loop that runs through each line in the file to find other instances of orders by user
+        for word in line.split(","):  
+            if word == userid:
+                order_count += 1
+
+    file.close()
+
+    file = open(filename, "a")   #opens orders.csv to append new order
+
+    file.write(userid + ", " + total + ", " + ordered_products + "\n")   #writes user order and total under userid
+    order_count += 1
+
+    print(order_count)
+
+    print("\n   FRI-12 Warehouse   ")   #print statements that print formatted reciept 
+    print(f"-" * 25)
+
+    for i in range(len(product_list)):
+        product_name = product_list[i][0]
+        product_price = float(product_list[i][1])
+        print(f"{product_name:<11}{'$' + format(float(product_price), ',.2f'):>15}")
+
+    print("-" * 25)
+    print(f"{'Subtotal':<10}{'$' + format(subtotal, ',.2f'):>15}")
+    print(f"{'Discount':<10}{'$' + format(discount, ',.2f'):>15}")
+    print(f"{'Tax':<10}{'$' + format(tax_applied, ',.2f'):>15}")
+    print(f"{'Total':<10}{'$' + format(float(total), ',.2f'):>15}")
+
+def customer_summary(userid):   #?Changes Required
+    file = open("products.csv",'r')
+    products = []
+
+    for line in file:
+        name = line.strip().split(",")
+        products.append(name[0])
+
+    file.close()
+   
+    product_count = [0]*len(products)
+
     validation = False
-    userlines = []
+    num_orders = 0
+    total_spent = 0
+
+    file = open("orders.csv",'r')
     for line in file:
         line = line.strip().split(",")
+
         if line[0] == userid:
             validation = True
-            userlines.append(line)
+            num_orders += 1
+            total_spent += float(line[1])
+
+            for i in range (len(products)):
+                for i2 in range (2,len(line)):
+                    if line[i2].strip() == products[i]:
+                        product_count[i] += 1
+
 
     if validation is True:
-        num_orders = len(userlines)  # total number of orders
-        total_spent = 0.0
-        product_count = [0, 0, 0, 0, 0, 0]
-        products = ["Sponge", "Bottle", "Rook", "D12", "Bowl", "WitchHat"]
-
-        for order in userlines:
-            product = str(order[1])
-            qty = int(order[2])
-            price = float(order[3])
-            total_spent += qty * price  # total spent of orders
-
-            for i in range(len(products)):  #store quantity of each product into the list
-                if product == products[i]:
-                    product_count[i] += qty
-
         print("===================================")
         print(f" Customer Summary for {userid}")
         print("===================================")
@@ -179,36 +404,36 @@ def customer_summary(userid):
         print("------------ Products -------------")
 
         for i in range(len(products)):
-            if product_count[i] != 0:   #print product quantity if not 0
-                print(f"{products[i]}             | {product_count[i]}")
-                
+            if product_count[i] != 0: #print product quantity if not 0
+                print(f"{products[i]:<15}| {product_count[i]}")
         print("===================================")
 
     else:
         print("Invalid username")
 
-while True:
-    ##Login the user  
-    user=authenticate()
-    if user==None:
-        print("Order canceled")
-        break
+##*Code starts here
+#Setting up the work directory to where ever the main.py is, useful for file paths
+location=os.path.dirname(__file__)
+os.chdir(location)
+##Welcome the user
+print('='*30)
+print("Welcome to Fri-12 Warehouse")
+print('='*30+'\n')
 
+##Login the user  
+user=authenticate()
+
+while True:
     ##Scans items   
-    # load_barcodes()
+    # scanned_list=BarcodeScanner.scan_barcode()    #!UNCOMMENT BEFORE DEMO
+    scanned_list="D12 Sponge WitchHat test" #!REMOVE BEFORE DEMO
 
     ##Looks up items and their prices
-    # product_list=lookup_products(scanned_list)
+    product_list=lookup_products(scanned_list)
+    print(f"product_list: {product_list}")
 
-    ##Order to Q-arm to move the items
-    product_list = []
-    file = open("products.csv", "r")
-    for line in file:
-        product = []
-        for word in line.split(","):
-            product.append(word)
-        product_list.append(product)
-    pack_products(product_list) 
+    ##Order the Q-arm to move the items
+    # pack_products(product_list)    #!UNCOMMENT BEFORE DEMO
 
     ##Calculate total & record order in csv
     complete_order(user,product_list)
@@ -218,4 +443,4 @@ while True:
         break
 
 ##Summarize customer order history
-#customer_summary(input("Please enter the user ID to see its account summary: "))   #!Skipped for now 
+customer_summary(user)
