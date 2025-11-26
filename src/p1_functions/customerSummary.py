96aa3d611aefb3d4f00618c168210435fe0bdb1f
ziruigu11-pixel <ziruigu11@gmail.com>
2025-11-26 17:37:47 -0500

    Update customerSummary.py

--- src/p1_functions/customerSummary.py
+++ src/p1_functions/customerSummary.py
@@ -4,11 +4,11 @@ def customer_summary(userid):
 
     for line in file:
         name = line.strip().split(",")
-        products.append(name[0])
+        products.append(name[0])        #add product names to products list
 
     file.close()
    
-    product_count = [0]*len(products)
+    product_count = [0]*len(products)        #create a list has the same length as products list to store number of product
 
     validation = False
     num_orders = 0
@@ -18,14 +18,14 @@ def customer_summary(userid):
     for line in file:
         line = line.strip().split(",")
 
-        if line[0] == userid:
+        if line[0] == userid:        #search for the userid that was asked
             validation = True
-            num_orders += 1
-            total_spent += float(line[1])
+            num_orders += 1        #plus one to number of order
+            total_spent += float(line[1])        #add the money spend on this order to total spend
 
             for i in range (len(products)):
-                for i2 in range (2,len(line)):
-                    if line[i2].strip() == products[i]:
+                for i2 in range (2,len(line)):        
+                    if line[i2].strip() == products[i]:        #search for product name from each valid line by reading product name list
                         product_count[i] += 1
 
 
@@ -38,10 +38,10 @@ def customer_summary(userid):
         print("------------ Products -------------")
 
         for i in range(len(products)):
-            if product_count[i] != 0: #print product quantity if not 0
+            if product_count[i] != 0:             #print product quantity if not 0
                 print(f"{products[i]:<15}| {product_count[i]}")
         print("===================================")
 
     else:
-        print("Invalid username")
+        print("Invalid username")        #if userid not found, invalid username
 
